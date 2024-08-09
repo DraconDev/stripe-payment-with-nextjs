813e15348f109df4e01a574bba9560bbfcdee4bc
DraconDev <dracsharp@gmail.com>
2024-08-09 12:48:41 +0100

    upd

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,18 +1,10 @@
 "use client";
 import { CartItem } from "@/types/cart";
 import Link from "next/link";
-import { useState } from "react";
 
 type Props = {};
 
 const Page = (props: Props) => {
-    const start = [
-        { quantity: 1, price: 1000 },
-        { quantity: 3, price: 2000 },
-        { quantity: 2, price: 1000 },
-    ];
-    const [items, setItems] = useState<CartItem[]>(start);
-
     const addItemToCart = (item: CartItem) => {
         // Get the current cart items from local storage
         const cartItems = localStorage.getItem("cart");
@@ -30,7 +22,6 @@ const Page = (props: Props) => {
 
     return (
         <div className="flex w-full flex-col justify-center text-center">
-            <div className="">Cart</div>
             <button onClick={() => addItemToCart({ quantity: 1, price: 1000 })}>
                 Add item
             </button>
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 export default function Home() {
     return (
         <main className="flex items-center  flex-col w-full ">
-            <div className="flex ">home</div>
             <Link className="flex " href="/cart">
                 Go go cart
             </Link>
